a41efeac2a61891989f6c313905d4ba6b90545e5
Carlos <hernandeasi7@gmail.com>
2020-12-17 03:06:20 -0300

    Primer Comit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+
+  mode: 'development',
+  optimization: {
+    minimizer: [ new OptimizeCssAssetsPlugin() ]
+},
+  //devtool: 'eval-source-map',
+  
+  module: {
+    rules: [
+
+      {
+        test: /\.css$/,
+        exclude: /styles\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+
+      {
+        test: /styles\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          attributes: false,
+          minimize: false
+        },
+
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false
+            }
+          }
+        ]
+      }
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      ignoreOrder: false
+    }),
+    new CopyPlugin({
+      patterns:[
+          { from: 'src/assets', to: 'assets/'}
+      ]
+  })
+
+  ]
+}
